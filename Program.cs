86ac77b0a86c1eaf46ace0d2ecc762642da7ab28
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-04-24 01:55:01 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace repetition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many Xs? ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            int counter=0;
+            while(num>0){
+                //yo dawg i heard you liked repetition so i put repetition in your repetition so this could loop forever if i did it wrong
+                while(num>counter){
+                    Console.Write('x');
+                    counter++;}
+                    Console.WriteLine();
+                    num--;
+                    counter=0;
+            }
+
+            {
+            Console.WriteLine("How many Lines?");
+            string lineimput = Console.ReadLine();
+            int lines = int.Parse(lineimput);
+            int baseline = 1;
+            while(lines>0){
+                counter=baseline; 
+                while(counter>0){
+                    Console.Write('x');
+                    counter--;}
+                    Console.WriteLine();
+                    baseline++;
+                    lines--;
+                }
+            }
+        }
+    }
+}
