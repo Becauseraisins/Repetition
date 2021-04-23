e8ecc98dd3fa61feb7f07a4e4ae5bc0212308637
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-04-24 01:57:00 +1000

    second commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestedRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Enter name: ");
+            String username= Console.ReadLine();
+            Console.Write("How many repeats? ");
+            String repeats = Console.ReadLine();
+            int counter = 0; 
+            
+            while (int.Parse(repeats)>counter){
+            Console.WriteLine(username);
+            counter++;
+            }
+            
+        }
+    }
+}
